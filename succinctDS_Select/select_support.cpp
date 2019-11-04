8bf90362dc6b23f104351f6871256d7da9983647
wangbixuan <bixuan.wang@brbiotech.com>
2019-11-03 22:35:39 -0500

    remove test comments

--- succinctDS_Select/select_support.cpp
+++ succinctDS_Select/select_support.cpp
@@ -13,15 +13,6 @@ select_support::select_support() {
 select_support::select_support(rank_support &r) {
     r_ref=r;
     r_N_value=r.get_N_value();
-    /*
-    r_S_value=r.get_S_value();
-    r_Rs_vec=r.get_Rs_vec();
-    size_Rs=r_Rs_vec.size();
-    r_B_value=r.get_B_value();
-    r_Rb_vec=r.get_Rb_vec();
-    r_Rp_vec=r.get_Rp_vec();
-
-     */
 }
 
 select_support::~select_support() {
@@ -34,18 +25,7 @@ uint64_t select_support::select0(uint64_t i) {
     uint64_t realpos=ceil((upper+lower)/2);
     uint64_t realrank=r_ref.rank0(realpos);
     uint64_t realrank_left=(realpos!=0) ? r_ref.rank0(realpos-1):-1;
-    /*
-    cout<<"realrankleft"<<realrank_left<<endl;
-    cout<<"realrank:"<<realrank<<endl;
-    cout<<"realpos"<<realpos<<endl;
-     */
     while((realrank!=i)||((realrank==i)&&(realrank==realrank_left))){
-        /*
-        cout<<"realpos:"<<realpos<<endl;
-        cout<<"realrank:"<<realrank<<endl;
-        cout<<"limit:"<<r_N_value<<endl;
-        cout<<"target:"<<i<<endl;
-         */
         if(realrank>i){
             upper=realpos;
         }else if(realrank<i){
@@ -63,12 +43,8 @@ uint64_t select_support::select0(uint64_t i) {
         }
 
         realpos=ceil((upper+lower)/2);
-        //cout<<"middle realpos:"<<realpos<<endl;
         realrank=r_ref.rank0(realpos);
-        //cout<<"middle realrank:"<<realrank<<endl;
         realrank_left=(realpos!=0) ? r_ref.rank0(realpos-1):-1;
-        //cout<<"endrealpos:"<<realpos<<endl;
-        //cout<<"endrealrank:"<<realrank<<endl;
     }
     return realpos;
 }
@@ -79,7 +55,6 @@ uint64_t select_support::select1(uint64_t i) {
     uint64_t realpos=ceil((upper+lower)/2);
     uint64_t realrank=r_ref.rank1(realpos);
     uint64_t realrank_left=(realpos!=0)? r_ref.rank1(realpos-1):-1;
-    //cout<<i<<endl;
     while((realrank!=i)||((realrank==i)&&(realrank==realrank_left))){
         if(realrank>i){
             upper=realpos;
